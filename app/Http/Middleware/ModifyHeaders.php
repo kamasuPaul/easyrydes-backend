9d8cd4f8d889bd1b7662d25fe166334bd171afea
Kamasu Paul <kamasupaul@gmail.com>
2022-04-15 09:48:11 +0300

    -- modify cors headers

--- app/Http/Middleware/ModifyHeaders.php
+++ app/Http/Middleware/ModifyHeaders.php
@@ -22,6 +22,7 @@ class ModifyHeaders
         $response = $next($request);
         // //fix cors error
         $response->header( 'Access-Control-Allow-Origin', '*' );
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         // $response->header( 'Access-Control-Allow-Headers','Content-Type','X-Auth-Token','Origin','Authorization');
 
         return $response;
